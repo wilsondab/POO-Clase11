3a9af77ac12f3f1628163a5e597a279295ce9821
Alejandro Jara <alejandroejara@hotmail.com>
2024-07-02 15:01:23 -0300

    Primer commit

---
+++ Ejercicio4Modulo3/Controllers/ConsignaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio4Modulo3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsignaController : ControllerBase
+    {
+
+        // Correr el script para tener los datos de la BD
+        // Se debe crear con la arquitectura en capas( el controller y services) para poder unicamente dar de alta un proveedor y recuperar todos los proveedores
+        // Todas las peticiones tienen que ser async
+        // Crear un middleware personalizado, que grabe en base de datos en la tabla logs cada interaccion que exista con los endpoints expuestos:
+            // Si hay un error en la peticion se debe grabar success = false  sino success = true
+            // para completar los datos de path y verbo http deben usar los metodos/propiedades
+            // de la variable "context" que se disponibiliza al implementar IMiddleware
+            
+    }
+}
